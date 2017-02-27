ee8e75c1c5c677ece1317a3ef6315c7013da982d
adoblas <adoblas@stratio.com>
2017-02-27 14:48:57 +0100

    Browser in test class

--- testsAT/src/test/java/com/stratio/qa/hol/testsAT/ITsample.java
+++ testsAT/src/test/java/com/stratio/qa/hol/testsAT/ITsample.java
@@ -1,13 +1,17 @@
 package com.stratio.qa.hol.testsAT;
 import com.stratio.qa.cucumber.testng.CucumberRunner;
+import com.stratio.qa.data.BrowsersDataProvider;
 import com.stratio.qa.hol.utils.BaseTest;
 import cucumber.api.CucumberOptions;
+import org.testng.annotations.Factory;
 import org.testng.annotations.Test;
 
 @CucumberOptions(features = { "src/test/resources/features/sample.feature" })
 public class ITsample extends BaseTest {
 
-    public ITsample() {
+    @Factory(enabled = false, dataProviderClass = BrowsersDataProvider.class, dataProvider = "availableUniqueBrowsers")
+    public ITsample(String browser) {
+        this.browser = browser;
     }
 
     @Test(enabled = true, groups = {"hol"})
